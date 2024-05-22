dff8c652b76f6b94e0673b30cba0585480fa643e
Patrick Darwinkel <patrick@darwinkel.net>
2024-05-22 10:15:03 +0200

    Add inference results

--- jobscripts/run_inference.sh
+++ jobscripts/run_inference.sh
@@ -10,6 +10,6 @@ module load Python/3.11.3-GCCcore-12.3.0
 #python3 -m venv $HOME/venvs/ltp
 #source $HOME/venvs/ltp/bin/activate
 
-export HF_HOME=/scratch/s3677923/hf_cache
+export HF_HOME=$TMPDIR/hf_cache
 
 python3 run_validation.py
