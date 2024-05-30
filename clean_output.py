19c3a1ca3b9be61dd8c360e306a3c71572d63324
Nexardus <seba270270270@gmail.com>
2024-05-30 15:30:12 +0200

    fixed alignment of true labels to inputs

--- clean_output.py
+++ clean_output.py
@@ -22,11 +22,16 @@ def extract_response(row, prompts):
 
     return response
 
+def normalize_text(text):
+    return ' '.join(text.lower().strip().split())
+
 if __name__ == "__main__":
     prompt_techniques = ["gcot", "logicot", "ccot"]
     prompts = read_prompts(prompt_techniques)
     df = pd.read_csv("./output/inference_output_21_may.csv", sep="\t")
     df['Cleared_output'] = df.apply(lambda row: extract_response(row, prompts), axis=1)
+    df['Original Input'] = df['Input']
+    df['Input'] = df['Input'].apply(normalize_text)
 
     extractor = LabelExtractor('fallacies.json')
 
@@ -35,8 +40,15 @@ if __name__ == "__main__":
     # df.to_csv("./output/inference_output_21_may_cleaned.csv", index=False, sep="\t")
 
     true_labels = pd.read_csv("datasets/unified_validation_set_downsampled.tsv", sep="\t")
+    true_labels['Input'] = true_labels['Input'].apply(normalize_text)
     true_labels.rename(columns={"MAFALDA Label": "True Label", "MAFALDA Superlabel": "True Superlabel"}, inplace=True)
+
     merged_df = df.merge(true_labels[['Input', 'True Label', 'True Superlabel']], on='Input', how='left')
+    merged_df['Input'] = merged_df['Original Input']
+    merged_df.drop(columns=['Original Input'], inplace=True)
+
+    # check if there are any NA values in the merged dataframe
+    print(merged_df.isna().sum()) # none
 
     models = merged_df['Model'].unique()
     for model in models:
