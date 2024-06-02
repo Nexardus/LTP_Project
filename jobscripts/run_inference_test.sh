04168966d17b276170f20c868b0bc3a7cdaeaa60
Patrick Darwinkel <patrick@darwinkel.net>
2024-06-02 10:48:53 +0200

    Run only hermes logicot for MAFALDA

--- jobscripts/run_inference_test.sh
+++ jobscripts/run_inference_test.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH --time=24:00:00
+#SBATCH --time=04:00:00
 #SBATCH --gpus-per-node=1
 #SBATCH --partition=gpu
 #SBATCH --mem=32000
--- run_test.py
+++ run_test.py
@@ -1,9 +1,8 @@
-import pandas as pd
-from run_validation import read_prompt, generate_all_models
+from run_validation import generate_all_models
 
 if __name__ == "__main__":
-    prompt_techniques = ["gcot", "logicot", "ccot", "multi-agent"]
-    models = ["Salesforce/xgen-7b-8k-base", "lmsys/vicuna-7b-v1.5", "NousResearch/Hermes-2-Pro-Llama-3-8B"]
+    prompt_techniques = ["logicot"]
+    models = ["NousResearch/Hermes-2-Pro-Llama-3-8B"]
     dataset_path = "datasets/MAFALDA_gold_processed.tsv"
     output_file = "output/MAFALDA_gold_output.csv"
     generate_all_models(models, prompt_techniques, dataset_path, output_file)
